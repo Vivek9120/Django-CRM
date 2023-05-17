eb0c7e0a6ead9b7b907ff9e92e1ba94a5171aea5
Vivek Kumar <vk84178@gmail.com>
2023-05-17 18:27:14 +0530

    adding login

--- website/urls.py
+++ website/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 
-urlpatterns = [
+urlpatterns = [ 
     path('',views.home, name='home'),
+    path('login/',views.login_user, name='login'),
+    path('logout/',views.logout_user, name='logout'),
+
 ]
