8963ed356f1ce356d4d69b99a1abbfa462b93a5e
Vivek Kumar <vk84178@gmail.com>
2023-05-21 17:43:54 +0530

    table added

--- website/views.py
+++ website/views.py
@@ -2,10 +2,13 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from .forms import SignUpForm
+from .models import Records
 # Create your views here.
 
 
 def home(request):
+    recordss= Records.objects.all()
+
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
@@ -18,7 +21,7 @@ def home(request):
             messages.success(request, 'There was some error while login in')
             return redirect('home')
     else:
-        return render(request, 'home.html', {})
+        return render(request, 'home.html', {'recordss':recordss })
 
 
 def login_user(request):
@@ -52,3 +55,14 @@ def register_user(request):
         return render(request,'register.html', {'form': form})
 
     return render(request, 'register.html', {'form':form})
+
+
+
+def customer_record(request,pk):
+    if request.user.is_authenticated:
+        customer_record= Records.objects.get(id=pk)
+        return render(request, 'record.html', {'customer_record':customer_record})
+    else:
+        messages.success(request, "You Must Be Logged In To View That Page...")
+        return redirect('home')
+
