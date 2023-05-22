3a73e63838ddb0fcb9749d429c1b184ea4a73b1d
Vivek Kumar <vk84178@gmail.com>
2023-05-22 13:46:49 +0530

    add record

--- website/models.py
+++ website/models.py
@@ -14,4 +14,4 @@ class Records(models.Model):
     def __str__(self):
         return (f"{self.first_name} {self.last_name}")
         
-    
+     
--- website/views.py
+++ website/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-from .forms import SignUpForm
+from .forms import SignUpForm,AddRecordForm
 from .models import Records
 # Create your views here.
 
@@ -34,9 +34,6 @@ def logout_user(request):
     return redirect('home')
 
 
-
-
-
 def register_user(request):
     if request.method == 'POST':
         form = SignUpForm(request.POST)
@@ -61,8 +58,31 @@ def register_user(request):
 def customer_record(request,pk):
     if request.user.is_authenticated:
         customer_record= Records.objects.get(id=pk)
-        return render(request, 'record.html', {'customer_record':customer_record})
+        return render(request, 'record.html', locals())
     else:
         messages.success(request, "You Must Be Logged In To View That Page...")
         return redirect('home')
 
+def delete_record(request , pk):
+    if request.user.is_authenticated:
+        delete_it = Records.objects.get(id=pk)
+        delete_it.delete()
+        messages.success(request, "Record Deleted Successfully...")
+        return redirect('home')
+    else:
+        messages.success(request, "You Must Be Logged In To Do That...")
+        return redirect('home')
+
+def add_record(request):
+    form = AddRecordForm(request.POST or None) 
+    if request.user.is_authenticated:
+        if request.method == "POST":
+            if form.is_valid():
+                add_record = form.save()
+                messages.success(request, "Record Added...")
+                return redirect('home')
+        return render(request, 'add_record.html', {'form':form})
+    else:
+        messages.success(request, "You Must Be Logged In...")
+        return redirect('home')
+    
